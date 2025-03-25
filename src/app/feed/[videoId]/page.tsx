42f34c63cd623933fd6764a6985df109684bcec5
Derrick <similienderrick@yahoo.com>
2025-03-25 01:49:35 -0400

    Initialized git repository

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+interface PageProps {
+    params: Promise <{ videoId: string }>;
+}
+
+
+const Page = async ({ params }: PageProps) => {
+ console.log("Server component");
+
+ const { videoId } = await params;
+
+  return (
+   <div>
+    Video Id: {videoId}
+   </div>
+  );
+}
+
+export default Page;
---
+++ src/app/feed/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+     <div>
+      Feed page!
+     </div>
+   );
+};
+
+export default Page; 
